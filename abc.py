f8db2e355bced281faf6788c9989b0f8025b6b0c
rishichandak1202 <107151249+rishichandak1202@users.noreply.github.com>
2022-06-09 02:07:00 +0530

    Add files via upload

---
+++ abc.py
@@ -0,0 +1,17 @@
+r = 1.1
+import math
+A = math.pi*r**2
+
+print ("The area of the circle with radius 1.1 is:", A)
+
+
+
+print ()
+
+
+Filename = ("abc.py")
+File_Extension = Filename.split(".")
+print ("Extension of the file is : " + File_Extension[+1])
+
+
+       
