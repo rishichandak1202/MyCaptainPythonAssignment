b7da3c50bb8c5bbec9166b433eac7a09d93d730d
rishichandak1202 <107151249+rishichandak1202@users.noreply.github.com>
2022-07-06 23:46:47 +0530

    Add files via upload

---
+++ marks.py
@@ -0,0 +1,12 @@
+marks = 89
+if marks >=90:
+    print ("A")
+elif marks >=80:
+    print ("B")
+elif marks >= 70:
+    print ("C")
+elif marks >= 60:
+    print ("D")
+else:
+    print ("Fail")
+    
